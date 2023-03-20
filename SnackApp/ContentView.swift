d1eea597771b08f3366ae9fc0e4402b83c8e9034
rsugaya <rsugaya@llc-noa.net>
2023-03-20 14:01:36 +0900

    Initial Commit

---
+++ SnackApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SnackApp
+//
+//  Created by 菅谷亮太 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnackApp/SnackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackAppApp.swift
+//  SnackApp
+//
+//  Created by 菅谷亮太 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SnackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
